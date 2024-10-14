6f995a0f8a00f7e2045d5a90076fe96056cdd829
lynrics30 <labutskupskups@gmail.com>
2024-10-14 08:40:35 +0800

    just change the word for tranasmit response in activity log

--- query/administrator/transmit_query.php
+++ query/administrator/transmit_query.php
@@ -10,11 +10,11 @@ $transmit_ID = $_POST['transmitGlobeID'];
 
 if ($transmit_Type == "paidGlobe") {
     mysqli_query($conn, "UPDATE globe_table SET final_status = 'TRANSMITTED' WHERE globe_id = '$transmit_ID'");
-    mysqli_query($conn, "INSERT INTO activity_log(network_type, remarks, network_date, role, action) VALUES ('GLOBE', '$role transmit in globe', '$date', '$role', 'TRANSMITTED')");
+    mysqli_query($conn, "INSERT INTO activity_log(network_type, remarks, network_date, role, action) VALUES ('GLOBE', '$role transmitted in globe', '$date', '$role', 'TRANSMITTED')");
 } else if ($transmit_Type == "paidSmart") {
     mysqli_query($conn, "UPDATE smart_table SET final_status = 'TRANSMITTED' WHERE smart_id = '$transmit_ID'");
-    mysqli_query($conn, "INSERT INTO activity_log(network_type, remarks, network_date, role, action) VALUES ('SMART', '$role transmit in smart', '$date', '$role', 'TRANSMITTED')");
+    mysqli_query($conn, "INSERT INTO activity_log(network_type, remarks, network_date, role, action) VALUES ('SMART', '$role transmitted in smart', '$date', '$role', 'TRANSMITTED')");
 } else {
     mysqli_query($conn, "UPDATE pldt_table SET final_status = 'TRANSMITTED' WHERE pldt_id = '$transmit_ID'");
-    mysqli_query($conn, "INSERT INTO activity_log(network_type, remarks, network_date, role, action) VALUES ('PLDT', '$role transmit in pldt', '$date', '$role', 'TRANSMITTED')");
+    mysqli_query($conn, "INSERT INTO activity_log(network_type, remarks, network_date, role, action) VALUES ('PLDT', '$role transmitted in pldt', '$date', '$role', 'TRANSMITTED')");
 };
