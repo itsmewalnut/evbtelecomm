47189de952832b9f5e842537a945a6cc4c25f383
lynrics30 <labutskupskups@gmail.com>
2024-12-02 08:45:55 +0800

    just changing the path of the logo

--- sidebar.php
+++ sidebar.php
@@ -31,7 +31,7 @@ if (array_key_exists($current_page, $active_class)) {
 <aside class="sidenav navbar navbar-vertical navbar-expand-xs border-0 border-radius-xl my-3 fixed-start ms-3 bg-gradient-dark" id="sidenav-main">
     <div class="sidenav-header">
         <i class="fas fa-times p-3 cursor-pointer text-white opacity-5 position-absolute end-0 top-0 d-none d-xl-none" aria-hidden="true" id="iconSidenav"></i>
-        <a class="navbar-brand m-0" href="dashboard.php">
+        <a class="navbar-brand m-0" href="dashboard">
             <div class="d-flex justify-content-center align-items-center">
                 <img src="../../image/HomeLogo.png" class="object-fit-cover w-75" alt="main_logo" />
             </div>
