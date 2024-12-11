46e182fcdd6abcc498db2c76646a7643214087d7
el1230 <labutskupskups@gmail.com>
2024-12-11 08:23:55 +0800

    just remove .php in home button

--- navbar.php
+++ navbar.php
@@ -23,7 +23,7 @@ $breadcrumb_title = isset($page_titles[$current_page]) ? $page_titles[$current_p
         <div class="container-fluid py-1 px-3">
             <nav aria-label="breadcrumb">
                 <ol class="breadcrumb bg-transparent mb-0 pb-0 pt-1 px-0 me-sm-6 me-5">
-                    <li class="breadcrumb-item text-sm"><a href="dashboard.php"><i class="material-icons opacity-10">home</i></a></li>
+                    <li class="breadcrumb-item text-sm"><a href="dashboard"><i class="material-icons opacity-10">home</i></a></li>
                     <li class="breadcrumb-item text-sm"><a class="opacity-5 text-dark" href="javascript:;">Pages</a></li>
                     <li class="breadcrumb-item text-sm text-dark active" aria-current="page"><?php echo $breadcrumb_title; ?></li>
                 </ol>
